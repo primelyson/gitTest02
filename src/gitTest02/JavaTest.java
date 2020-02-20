50545dde4e8769e0fad5b7327a79a209dd2a4a31
primelyson <55792638+primelyson@users.noreply.github.com>
2020-02-20 22:53:52 +0900

    Update JavaTest.java

--- src/gitTest02/JavaTest.java
+++ src/gitTest02/JavaTest.java
@@ -3,7 +3,7 @@ package gitTest02;
 public class JavaTest {
 
 	public static void main(String[] args) {
-		System.out.println("111111111111111");
+		System.out.println("111111111111111222222");
 	}
 
 }
